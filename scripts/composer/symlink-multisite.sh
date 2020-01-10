8d83e9694e4e5c0156a9055d7687a93e05ea86e3
Aleksi Peebles <aleksi@iki.fi>
2020-01-10 15:24:04 +0200

    Update for Drupal 8.8.x

--- scripts/composer/symlink-multisite.sh
+++ scripts/composer/symlink-multisite.sh
@@ -3,19 +3,20 @@
 SITES_PATH=web/sites
 MULTISITES_PATH=../../../../../multisites
 
-symlink_site() {
-  mkdir -p ${SITES_PATH}/${1}
-  if [ ! -L ${SITES_PATH}/${1}/settings.php ] && [ ! -e ${SITES_PATH}/${1}/settings.php ]; then
-    ln -s ${MULTISITES_PATH}/${1}/settings.php ${SITES_PATH}/${1}/settings.php
+symlink_site ()
+{
+  mkdir -p "${SITES_PATH}/${1}"
+  if [ ! -e "${SITES_PATH}/${1}/settings.php" ]; then
+    ln -s "${MULTISITES_PATH}/${1}/settings.php" "${SITES_PATH}/${1}/settings.php"
   fi
-  if [ ! -L ${SITES_PATH}/${1}/settings.multisite.php ] && [ ! -e ${SITES_PATH}/${1}/settings.multisite.php ]; then
-    ln -s ${MULTISITES_PATH}/${1}/settings.multisite.php ${SITES_PATH}/${1}/settings.multisite.php
+  if [ ! -e "${SITES_PATH}/${1}/settings.multisite.php" ]; then
+    ln -s "${MULTISITES_PATH}/${1}/settings.multisite.php" "${SITES_PATH}/${1}/settings.multisite.php"
   fi
-  if [ ! -L ${SITES_PATH}/${1}/themes ] && [ ! -e ${SITES_PATH}/${1}/themes ]; then
-    ln -s ${MULTISITES_PATH}/${1}/themes ${SITES_PATH}/${1}/themes
+  if [ ! -e "${SITES_PATH}/${1}/themes" ]; then
+    ln -s "${MULTISITES_PATH}/${1}/themes" "${SITES_PATH}/${1}/themes"
   fi
-  if [ ! -L ${SITES_PATH}/${1}/modules ] && [ ! -e ${SITES_PATH}/${1}/modules ]; then
-    ln -s ${MULTISITES_PATH}/${1}/modules ${SITES_PATH}/${1}/modules
+  if [ ! -e "${SITES_PATH}/${1}/modules" ]; then
+    ln -s "${MULTISITES_PATH}/${1}/modules" "${SITES_PATH}/${1}/modules"
   fi
 }
 
--- scripts/composer/symlink.sh
+++ scripts/composer/symlink.sh
@@ -2,13 +2,13 @@
 
 SITE_DIR=web/sites/www.shila.test
 
-mkdir -p ${SITE_DIR}
-if [ ! -L ${SITE_DIR}/settings.php ] && [ ! -e ${SITE_DIR}/settings.php ]; then
-  ln -s ../../../settings.php ${SITE_DIR}/settings.php
+mkdir -p "${SITE_DIR}"
+if [ ! -e "${SITE_DIR}/settings.php" ]; then
+  ln -s ../../../settings.php "${SITE_DIR}/settings.php"
 fi
-if [ ! -L ${SITE_DIR}/themes ] && [ ! -e ${SITE_DIR}/themes ]; then
-  ln -s ../../../themes ${SITE_DIR}/themes
+if [ ! -e "${SITE_DIR}/themes" ]; then
+  ln -s ../../../themes "${SITE_DIR}/themes"
 fi
-if [ ! -L ${SITE_DIR}/modules ] && [ ! -e ${SITE_DIR}/modules ]; then
-  ln -s ../../../modules ${SITE_DIR}/modules
+if [ ! -e "${SITE_DIR}/modules" ]; then
+  ln -s ../../../modules "${SITE_DIR}/modules"
 fi
